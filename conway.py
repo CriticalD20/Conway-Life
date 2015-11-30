b084123bc53028749ac2db25abaa0e0de6e285b9
CriticalD20 <jamesnapier@hanovernorwichschools.org>
2015-11-30 14:56:52 -0500

    Updated from Brython Server: 11/30/2015 2:56:52 PM

--- conway.py
+++ conway.py
@@ -5,4 +5,16 @@ Credit: <list sources used, if any>
 Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
+
+
+Reference Morgan's Code Here: https://github.com/morganmeliment/Conway-Life/blob/master/conway.py
 """
+
+
+from ggame import App, Color, Sprite, RectangleAsset, LineStyle, MouseEvent
+
+
+class Cell(Sprite):
+    
+
+class Conway(App):
